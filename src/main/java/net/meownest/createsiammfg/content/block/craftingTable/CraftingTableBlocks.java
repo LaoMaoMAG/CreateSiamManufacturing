4c62a9ee66500c7b20163d0a5f806a4e5294fd63
laomaomag <laomaomag@qq.com>
2026-09-26 21:03:32 +0800

    update

---
+++ src/main/java/net/meownest/createsiammfg/content/block/craftingTable/CraftingTableBlocks.java
@@ -0,0 +1,9 @@
+package net.meownest.createsiammfg.content.block.craftingTable;
+
+import net.minecraft.world.level.block.Block;
+
+public class CraftingTableBlocks extends Block {
+    public CraftingTableBlocks(Properties properties) {
+        super(properties);
+    }
+}
--- src/main/java/net/meownest/createsiammfg/registry/block/CSMBlocks.java
+++ src/main/java/net/meownest/createsiammfg/registry/block/CSMBlocks.java
@@ -1,12 +1,20 @@
 package net.meownest.createsiammfg.registry.block;
 
+import net.meownest.createsiammfg.content.block.craftingTable.CraftingTableBlocks;
+import net.meownest.createsiammfg.registry.CSMRegistrate;
+
 import com.simibubi.create.foundation.data.CreateRegistrate;
 import com.tterrag.registrate.util.entry.BlockEntry;
-import net.meownest.createsiammfg.registry.CSMRegistrate;
 
 /**
  * CMS 方块注册类
  */
 public class CSMBlocks {
     private static final CreateRegistrate REGISTRATE = CSMRegistrate.REGISTRATE;
+
+    /**
+     * 手工台
+     */
+    public static final BlockEntry<?> CRAFTING_TABLE = REGISTRATE.block("crafting_table", CraftingTableBlocks::new)
+            .register();
 }
